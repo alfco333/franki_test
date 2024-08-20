43b93c199e8601609d42d1b7b3d3db16403cfb63
Angel Vargas <angel.vargas@globant.com>
2024-08-19 23:46:59 -0600

    Franki test: Fix the loading view
    * Make the CircularProgressBar a little more stylish.

--- app/src/main/java/com/angelvargas/frankitest/WeatherScreen.kt
+++ app/src/main/java/com/angelvargas/frankitest/WeatherScreen.kt
@@ -4,14 +4,17 @@ package com.angelvargas.frankitest
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.repeatOnLifecycle
 
@@ -52,7 +55,13 @@ fun WeatherScreen(weatherViewModel: WeatherViewModel) {
             }
         }
         is WeatherUiState.Loading -> {
-            CircularProgressIndicator()
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator(modifier = Modifier.size(120.dp))
+            }
         }
 
         is WeatherUiState.Error -> {
